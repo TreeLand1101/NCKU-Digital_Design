bfbf2a7c50900244d1c19eb5473f86067a0b7269
TreeLand1101 <cowseer60906@gmail.com>
2024-05-04 22:54:33 +0800

    feat: Update MM.v

--- HW3_P76124265/RTL/MM.v
+++ HW3_P76124265/RTL/MM.v
@@ -11,23 +11,20 @@ input           row_end;
 input      [7:0]     in_data;
 
 output reg signed [19:0]   out_data;
-output is_legal;
+output reg is_legal;
 output reg change_row,valid,busy;
 
 // State
-parameter [2:0] Load_Mat1 = 3'b000, Load_Mat2 = 3'b001, Mat_Mul = 3'b010, Illegal = 3'b011, Validation = 3'b100, Done = 3'b101;
+parameter [2:0] Load_Mat1 = 3'b000, Load_Mat2 = 3'b001, Mat_Mul = 3'b010, Clear = 3'b011, Illegal = 3'b100, Done = 3'b101;
 reg [2:0] Current_State, Next_State;
 
 // Matrix 1
-reg signed [7:0] Mat1[20:0];
-reg [3:0] Mat1_Row, Mat1_Col, Mat1_Cur_Row, Mat1_Cur_Col;
+reg signed [7:0] Mat1[3:0][3:0];
+reg [2:0] Mat1_Row, Mat1_Col, Mat1_Cur_Row, Mat1_Cur_Col;
 
 // Matrix 2
-reg signed [7:0] Mat2[20:0];
-reg [3:0] Mat2_Row, Mat2_Col, Mat2_Cur_Row, Mat2_Cur_Col;
-
-reg [3:0] i;
-reg [3:0] Out_Data_Cnt;
+reg signed [7:0] Mat2[3:0][3:0];
+reg [2:0] Mat2_Row, Mat2_Col, Mat2_Cur_Row, Mat2_Cur_Col;
 
 // State Register
 always @(posedge clk or posedge rst) 
@@ -39,7 +36,7 @@ begin
 end
 
 // Next State
-always @(Current_State or row_end or Out_Data_Cnt) 
+always @(*) 
 begin
     case (Current_State)
         Load_Mat1: begin
@@ -51,23 +48,25 @@ begin
 
         Load_Mat2: begin
             if (row_end) begin
-                if ((Mat1_Row != Mat2_Col) || (Mat1_Col != Mat2_Row)) 
-                    Next_State <= Illegal;
-                else
+                if (Mat1_Col == Mat2_Row + 1) 
                     Next_State <= Mat_Mul;
+                else
+                    Next_State <= Illegal;
             end
             else 
                 Next_State <= Load_Mat2;
         end
      
         Mat_Mul: begin
-            if (Out_Data_Cnt > 0)
-                Next_State <= Validation;
-            else
+            if ((Mat1_Cur_Row == Mat1_Row - 1) && (Mat1_Cur_Col == Mat1_Col - 1) && (Mat2_Cur_Row == Mat2_Row - 1) && (Mat2_Cur_Col == Mat2_Col - 1))
                 Next_State <= Done;
+            else if (Mat1_Cur_Col == Mat1_Col - 1)
+                Next_State <= Clear;
+            else
+                Next_State <= Mat_Mul;
         end
 
-        Validation: begin
+        Clear: begin
             Next_State <= Mat_Mul;
         end
 
@@ -92,46 +91,83 @@ always @(posedge clk or posedge rst) begin
         Mat2_Col <= 0;
         Mat2_Cur_Row <= 0;
         Mat2_Cur_Col <= 0;
-        i <= 0;
-        Out_Data_Cnt <= 0;
         out_data <= 0;
+        is_legal <= 0;
+        change_row <= 0;
         valid <= 0;
         busy <= 0;
     end
     else begin
         case (Current_State)
             Load_Mat1: begin
-                Mat1[i] <= in_data; 
+                Mat1[Mat1_Row][Mat1_Col] <= in_data; 
                 if (col_end) begin
                     Mat1_Row <= Mat1_Row + 1;
-                    Mat1_Col <= i + 1;
+                    Mat1_Col <= 0;
                 end
-                if (row_end) 
-                    i <= 0;
                 else
-                    i <= i + 1;
+                    Mat1_Col <= Mat1_Col + 1;
+
+                if (row_end) begin
+                    Mat1_Row <= Mat1_Row + 1;
+                    Mat1_Col <= Mat1_Col + 1;                  
+                end
             end
 
             Load_Mat2: begin
-                Mat2[i] <= in_data; 
+                Mat2[Mat2_Row][Mat2_Col] <= in_data; 
                 if (col_end) begin
                     Mat2_Row <= Mat2_Row + 1;
-                    Mat2_Col <= i + 1;
-                end
-                if (row_end) begin
-                    busy <= 1;
-                    Out_Data_Cnt <= i;
+                    Mat2_Col <= 0;
                 end
                 else
-                    i <= i + 1;
+                    Mat2_Col <= Mat2_Col + 1;
+
+                if (row_end) begin
+                    busy <= 1;
+                    Mat2_Row <= Mat2_Row + 1;
+                    Mat2_Col <= Mat2_Col + 1;
+                end
             end
 
             Mat_Mul: begin
-                out_data <= out_data + Mat1[Mat1_Cur_Row * Mat1_Col + Mat1_Cur_Col] * Mat2[Mat2_Cur_Row * Mat2_Col + Mat2_Cur_Col];
-                Out_Data_Cnt <= Out_Data_Cnt - 1;
+                out_data <= out_data + Mat1[Mat1_Cur_Row][Mat1_Cur_Col] * Mat2[Mat2_Cur_Row][Mat2_Cur_Col];
+
+                if ((Mat2_Cur_Row == Mat2_Row - 1) && (Mat2_Cur_Col == Mat2_Col - 1)) begin
+                    change_row <= 1;
+                    Mat1_Cur_Row <= Mat1_Cur_Row + 1;
+                end
+                else
+                    change_row <= 0;
+
+                if ((Mat1_Cur_Col == Mat1_Col - 1) && (Mat2_Cur_Row == Mat2_Row - 1)) begin
+                    is_legal <= 1;
+                    valid <= 1;                    
+                    if (Mat2_Cur_Col == Mat2_Col - 1)
+                        Mat2_Cur_Col <= 0;
+                    else
+                        Mat2_Cur_Col <= Mat2_Cur_Col + 1;
+                end
+                else begin
+                    is_legal <= 0;
+                    valid <= 0;
+                end
+
+                if (Mat1_Cur_Col == Mat1_Col - 1)
+                    Mat1_Cur_Col <= 0;
+                else
+                    Mat1_Cur_Col <= Mat1_Cur_Col + 1;
+
+                if (Mat2_Cur_Row == Mat2_Row - 1)
+                    Mat2_Cur_Row <= 0;
+                else
+                    Mat2_Cur_Row <= Mat2_Cur_Row + 1;
             end
 
-            Validation: begin
+            Clear: begin
+                out_data <= 0;
+                is_legal <= 0;
+                change_row <= 0;
                 valid <= 0;
             end
 
@@ -148,9 +184,9 @@ always @(posedge clk or posedge rst) begin
                 Mat2_Col <= 0;
                 Mat2_Cur_Row <= 0;
                 Mat2_Cur_Col <= 0;
-                i <= 0;
-                Out_Data_Cnt <= 0;
                 out_data <= 0;
+                is_legal <= 0;
+                change_row <= 0;
                 valid <= 0;
                 busy <= 0;
             end
