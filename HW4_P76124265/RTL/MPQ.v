5ac1c371c0059be76cf94d21a6d6e25af1dbf657
TreeLand1101 <cowseer60906@gmail.com>
2024-05-19 17:12:10 +0800

    feat: Update MPQ.v

--- HW4_P76124265/RTL/MPQ.v
+++ HW4_P76124265/RTL/MPQ.v
@@ -1,7 +1,6 @@
 // 
 // Designer: P76124265
 //
-`include "Functions.v"
 
 module MPQ(clk,rst,data_valid,data,cmd_valid,cmd,index,value,busy,RAM_valid,RAM_A,RAM_D,done);
 input clk;
@@ -18,40 +17,27 @@ output reg [7:0]RAM_A;
 output reg [7:0]RAM_D;
 output reg done;
 
-parameter [1:0] State_Bit = 2;
-parameter [State_Bit - 1:0] Load = 0;
-parameter [State_Bit - 1:0] Read_Cmd = 1;
-parameter [State_Bit - 1:0] Execute_Cmd = 2;
-parameter [State_Bit - 1:0] Done = 3;
+parameter [2:0] Load = 3'b000, Read_Cmd = 3'b001, Execute_Cmd = 3'b010, Done = 3'b011;
+reg [2:0] Current_State, Next_State;
 
-parameter [2:0] Command_Bit = 2;
-parameter [Command_Bit - 1:0] Build_Queue = 0;
-parameter [Command_Bit - 1:0] Extract_Max = 1;
-parameter [Command_Bit - 1:0] Increase_Value = 2;
-parameter [Command_Bit - 1:0] Insert_Data = 3;
-parameter [Command_Bit - 1:0] Write = 4;
-
-reg [7:0] Size;
-reg [7:0] Array [7:0];
-reg [7:0] i;
+parameter [2:0] Build_Queue = 3'b000, Extract_Max = 3'b001, Increase_Value = 3'b010, Insert_Data = 3'b011, Write = 3'b100;
 
 reg Build_Queue_Enable;
 reg Extract_Max_Enable;
 reg Increase_Value_Enable;
 reg Insert_Data_Enable;
-reg Max_Heapify_Enable;
+reg Write_Enable;
 
-reg Build_Queue_Done;
-reg Extract_Max_Done;
-reg Increase_Value_Done;
-reg Insert_Data_Done;
-reg Max_Heapify_Done;
+reg [7:0] Size;
+reg [7:0] Array [255:0];
+reg [7:0] i, i_temp;
+wire [7:0] l, r;
+reg [7:0] largest;
+reg Max_Heapify_Running;
+reg Command_Done;
 
-Build_Queue Build_Queue(.Array(Array), .Size(Size), .enble(Build_Queue_Enable), .done(Build_Queue_Done));
-Extract_Max Extract_Max(.Array(Array), .Size(Size), .enble(Extract_Max_Enable), .done(Extract_Max_Done));
-Increase_Value Increase_Value(.Array(Array), .Size(Size), .index(index), .value(value), .enble(Increase_Value_Enable), .done(Increase_Value_Done));
-Insert_Data Insert_Data(.Array(Array), .Size(Size), .value(value), .enble(Insert_Data_Enable), .done(Insert_Data_Done));
-Max_Heapify Max_Heapify(.Array(Array), .Size(Size), .i(i), .enble(Max_Heapify_Enable), .done(Max_Heapify_Done));
+assign l = (i << 1) + 1;
+assign r = (i << 1) + 2;
 
 // State Register
 always @(posedge clk or posedge rst) 
@@ -68,37 +54,42 @@ begin
     case (Current_State)
         Load: begin
             if (data_valid)
-                Next_State <= Load;
+                Next_State = Load;
             else
-                Next_State <= Read_Cmd;
+                Next_State = Done;  
         end
 
         Read_Cmd: begin
-            Next_State <= Execute_Cmd;
+            Next_State = Execute_Cmd;
         end
      
         Execute_Cmd: begin
-            if (done)
-                Next_State <= Done;
+            if (Command_Done)
+                Next_State = Done;
             else
-                Next_State <= Execute_Cmd;
+                Next_State = Execute_Cmd;
         end  
 
         Done: begin
-            Next_State <= Load;
+            Next_State = Read_Cmd;
         end
     endcase
 end
 
-// Output logic && Datapath
+// Output && Datapath
 always @(posedge clk or posedge rst) 
 begin
     if (rst) begin
+        RAM_valid <= 0;
         RAM_A <= 0;
         RAM_D <= 0;
-        busy <= 0;
+        busy <= 1;
         done <= 0;
         Size <= 0;
+        i <= 0;
+        i_temp <= 0;
+        Command_Done <= 0;
+        Max_Heapify_Running <= 0;
         Build_Queue_Enable <= 0;
         Extract_Max_Enable <= 0;
         Increase_Value_Enable <= 0;
@@ -108,39 +99,96 @@ begin
     else begin
         case (Current_State)
             Load: begin
-                Array[Size] <= in_data;
-                Size <= Size + 1;
+                if (data_valid) begin
+                    Array[Size] <= data;
+                    Size <= Size + 1;
+                end
             end
 
             Read_Cmd: begin
+                case (cmd)
+                    Build_Queue: begin
+                        Build_Queue_Enable <= 1;
+                        i_temp <= (Size >> 1);
+                        i <= (Size >> 1);
+                    end
+                    Extract_Max: begin
+                        Extract_Max_Enable <= 1;
+                        i_temp <= Size;
+                    end
+                    Increase_Value: begin
+                        Increase_Value_Enable <= 1;
+                    end
+                    Insert_Data: begin
+                        Insert_Data_Enable <= 1;
+                    end
+                    Write: begin
+                        Write_Enable <= 1;
+                        i_temp <= 0;
+                    end
+                endcase
                 busy <= 1;
             end
         
             Execute_Cmd: begin
-                case (cmd)
-                    Build_Queue: Build_Queue_Enable <= 1;
-                    Extract_Max: Extract_Max_Enable <= 1;
-                    Increase_Value: Increase_Value_Enable <= 1;
-                    Insert_Data: Insert_Data_Enable <= 1;
-                    Write: begin
-                        if (RAM_A < Size) begin
-                            RAM_A <= RAM_A + 1;
-                            RAM_D <= Array[RAM_A];
+                if (Build_Queue_Enable) begin
+                    if (!Max_Heapify_Running) begin
+                        if (i_temp == 0) begin
+                            Command_Done <= 1;
                         end
                         else begin
-                            busy <= 0;
-                            done <= 1;
+                            Max_Heapify_Running <= 1;
+                            i_temp <= i_temp - 1;
+                            i <= i_temp - 1;
                         end
                     end
-                endcase
+                end
+                else if (Extract_Max_Enable) begin
+                    if ((Size < 1) || (!Max_Heapify_Running && (i_temp - 1 == Size)))
+                        Command_Done <= 1;
+                    else if(!Max_Heapify_Running) begin
+                        Array[1] = Array[Size - 1];
+                        Size = Size - 1;
+                        i <= 1;
+                        Max_Heapify_Running <= 1;
+                    end
+                    else
+                        Max_Heapify_Running <= 1;
+                end
+                else if (Increase_Value_Enable) begin
+                    
+                end
+                else if (Insert_Data_Enable) begin
+
+                end
+                else if (Write_Enable) begin
+                    if (i_temp < Size) begin
+                        RAM_valid <= 1;
+                        RAM_D <= Array[i_temp];
+                        i_temp <= i_temp + 1;
+                        if (RAM_valid)
+                            RAM_A <= i_temp;
+                    end
+                    else begin
+                        Write_Enable <= 0;
+                        RAM_valid <= 0;
+                        Command_Done <= 1;
+                        done <= 1;
+                    end
+                end
+                busy <= 1;
             end
 
             Done: begin
+                RAM_valid <= 0;
                 RAM_A <= 0;
                 RAM_D <= 0;
                 busy <= 0;
                 done <= 0;
-                Size <= 0;
+                i <= 0;
+                i_temp <= 0;
+                Command_Done <= 0;
+                Max_Heapify_Running <= 0;    
                 Build_Queue_Enable <= 0;
                 Extract_Max_Enable <= 0;
                 Increase_Value_Enable <= 0;
@@ -148,6 +196,26 @@ begin
                 Write_Enable <= 0;
             end
         endcase
+    end
+end
+
+// Max_Heapify
+always @(posedge clk or posedge rst) 
+begin
+    if (Max_Heapify_Running) begin
+        if ((l >= Size || Array[l] <= Array[i]) && (r >= Size || Array[r] <= Array[i])) begin
+            Max_Heapify_Running <= 0;
+        end
+        else if ((l < Size) && (Array[l] > Array[i]) && (r >= Size || Array[r] <= Array[l])) begin
+            i <= l;
+            Array[l] <= Array[i];
+            Array[i] <= Array[l];
+        end
+        else begin
+            i <= r;
+            Array[r] <= Array[i];
+            Array[i] <= Array[r];
+        end
     end
 end
 
