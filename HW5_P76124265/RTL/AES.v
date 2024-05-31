71d9d7db75877096da8f8a0a868818e6dd95034f
LSC <37861737+LSC-treeland@users.noreply.github.com>
2024-05-31 17:08:11 +0800

    feat: Updates Files

--- HW5_P76124265/RTL/AES.v
+++ HW5_P76124265/RTL/AES.v
@@ -14,11 +14,26 @@ module AES(
 // write your design here //
 
 parameter round = 10;
-wire [(128 * (round + 1)) - 1 :0] fullkeys;
+wire [127:0] fullkeys [round + 1:0];
 wire [127:0] states [round + 1:0];
 wire [127:0] afterSubBytes;
 wire [127:0] afterShiftRows;
 
+// keyExpansion KE(key, fullkeys);
 
+// addRoundKey addRK1 (in, states[0], fullkeys[((128*(Nr+1))-1)-:128]);
+
+// genvar i;
+// generate
+	
+// 	for(i = 1; i < round; i = i + 1)
+// 		encryptRound er(states[i-1],fullkeys[(((128*(Nr+1))-1)-128*i)-:128],states[i]);
+
+// 	subBytes sb(states[Nr-1],afterSubBytes);
+// 	shiftRows sr(afterSubBytes,afterShiftRows);
+// 	addRoundKey addrk2(afterShiftRows,states[Nr],fullkeys[127:0]);
+// 	assign out=states[Nr];
+
+// endgenerate
 
 endmodule
