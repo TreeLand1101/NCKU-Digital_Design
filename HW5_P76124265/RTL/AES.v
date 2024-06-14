6ce91e3df87b298274cf4c731d3c1221f076f1cd
TreeLand1101 <cowseer60906@gmail.com>
2024-06-14 22:55:01 +0800

    feat: Update Files

--- HW5_P76124265/RTL/AES.v
+++ HW5_P76124265/RTL/AES.v
@@ -14,36 +14,44 @@ module AES(
 // write your design here //
 
 parameter round = 10;
-wire [127:0] fullkeys [round:0];
-wire [127:0] states [round:0];
+reg [127:0] fullkeys [round - 1:0];
+reg [127:0] states [round - 1:0];
+wire [127:0] fullkeysTemp [round - 1:0];
+wire [127:0] statesTemp [round - 1:0];
+wire [127:0] result; 
 
-addRoundKey addRK1 (P, K, states[0]);
-
-assign fullkeys[0] = K;
+addRoundKey addRK1 (P, K, statesTemp[0]);
+keyExpansion KE(K, 1, fullkeysTemp[0]);
 
 genvar i;
 generate
         
     for(i = 1; i < round; i = i + 1) begin : AES_loop
-        keyExpansion KE(fullkeys[i - 1], i, fullkeys[i]);
-        encryptRound ER(states[i - 1], fullkeys[i], states[i]);
+        keyExpansion KE(fullkeys[i - 1], i + 1, fullkeysTemp[i]);
+        encryptRound ER(states[i - 1], fullkeys[i - 1], statesTemp[i]);
     end
-    keyExpansion KE(fullkeys[round - 1], round, fullkeys[round]);
-    lastEncryptRound LER(states[round - 1], fullkeys[round], states[round]);
+    lastEncryptRound LER(states[round - 1], fullkeys[round - 1], result);
 
 endgenerate
+
+integer j;
 
 always@(posedge clk or posedge rst) begin
     if(rst) begin
         valid <= 0;
     end
     else begin
-        if(states[round] !== 128'hx)
+        for(j = 0; j < round; j = j + 1) begin : assign_loop
+            fullkeys[j] <= fullkeysTemp[j];
+            states[j] <= statesTemp[j];
+        end        
+
+        if(result !== 128'hx) begin
+            C <= result;
             valid <= 1;
+        end
         else
             valid <= 0;
-
-        C <= states[round];      
     end
 end
 
